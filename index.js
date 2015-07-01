b1b4e898e4e8a7be2a2250da8cdd7a2f5769e323
Yoshua Wuyts <i@yoshuawuyts.com>
2015-07-01 10:35:15 +0200

    marked -> mdast

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
+const html = require('mdast-html')
 const assert = require('assert')
-const marked = require('marked')
-const clone = require('clone')
+const mdast = require('mdast')
 
-module.exports = toObj
+module.exports = mdjson
 
 // map a markdown string to an object
 // with `html` and `raw` fields
 // str -> obj
-function toObj (txt) {
+function mdjson (txt) {
   assert.equal(typeof txt, 'string', 'input should be a markdown string')
-  const lexer = new marked.Lexer()
-  const tokens = lexer.lex(txt)
-  // const parsed = marked.parser(clone(tokens)).split('\n')
+  const toHtml = mdast().use(html)
+  const lexer = mdast()
+
+  const tokens = lexer.parse(txt).children
   const res = {}
   var key = ''
 
-  console.log(marked.parser(clone(tokens)).length)
-
   tokens.forEach(function (token, i) {
     if (token.type === 'heading') {
-      key = token.text
+      key = token.children[0].value
       const html = []
       html.links = true
       res[key] = {html: html, raw: []}
@@ -28,13 +27,13 @@ function toObj (txt) {
 
     if (!key) return
 
-    res[key].raw.push(token.text)
+    res[key].raw.push(lexer.stringify(token))
     res[key].html.push(token)
   })
 
   Object.keys(res).forEach(function (key) {
     res[key].raw = res[key].raw.join('\n')
-    res[key].html = marked.parser(clone(res[key].html)).trim()
+    res[key].html = toHtml.process(res[key].raw).trim()
   })
 
   return res
